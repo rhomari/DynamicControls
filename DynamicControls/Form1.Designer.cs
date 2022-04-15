d57e7769cd34ee6c8dbfb53518996f945c051d28
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-04-15 22:06:27 +0000

    Ajoutez des fichiers projet.

---
+++ DynamicControls/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace DynamicControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.ButtonsPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTEXT = new System.Windows.Forms.TextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.cbxCOLOR = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(211, 16);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(573, 410);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.AutoScroll = true;
+            this.ButtonsPanel.Location = new System.Drawing.Point(9, 16);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.Size = new System.Drawing.Size(189, 409);
+            this.ButtonsPanel.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add new shortcut";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTEXT
+            // 
+            this.txtTEXT.Location = new System.Drawing.Point(458, 449);
+            this.txtTEXT.Name = "txtTEXT";
+            this.txtTEXT.Size = new System.Drawing.Size(177, 23);
+            this.txtTEXT.TabIndex = 100;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(458, 478);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(177, 23);
+            this.txtURL.TabIndex = 101;
+            // 
+            // cbxCOLOR
+            // 
+            this.cbxCOLOR.FormattingEnabled = true;
+            this.cbxCOLOR.Location = new System.Drawing.Point(458, 508);
+            this.cbxCOLOR.Name = "cbxCOLOR";
+            this.cbxCOLOR.Size = new System.Drawing.Size(177, 23);
+            this.cbxCOLOR.TabIndex = 103;
+            this.cbxCOLOR.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Text :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 481);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Link :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(396, 511);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 538);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCOLOR);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.txtTEXT);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonsPanel);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Panel ButtonsPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTEXT;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ComboBox cbxCOLOR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DynamicControls/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DynamicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webView.EnsureCoreWebView2Async(null);
+            fillColors();
+            createButtons();
+        }
+        private void fillColors()
+        {
+            foreach (System.Reflection.PropertyInfo propriete in typeof(Color).GetProperties())
+            {
+                if (propriete.PropertyType.FullName == "System.Drawing.Color")
+                {
+                    cbxCOLOR.Items.Add(propriete.Name);
+                   
+                }
+                    
+                    
+            }
+        }
+
+       
+        private void createButtons()
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(Application.StartupPath + "\\Buttons.xml");
+            DataTable dataTable = dataSet.Tables[0];
+            int top = 10;
+            ButtonsPanel.Controls.Clear();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                //dynamically creating Buttons from XML file
+                Button button = new Button();
+                button.Text = dataRow["TEXT"].ToString();
+                button.BackColor = Color.FromName(dataRow["COLOR"].ToString());
+                button.ForeColor = Color.White;
+                this.ButtonsPanel.Controls.Add(button);
+                button.Location = new System.Drawing.Point(20, top);
+                button.Size = new System.Drawing.Size(130, 30);
+                button.Click += new EventHandler((obj, Event) =>
+                {
+                    webView.CoreWebView2.Navigate(dataRow["URL"].ToString());
+                });
+                top += 30;
+            }
+            
+            
+
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtTEXT.Text!=string.Empty && txtURL.Text != string.Empty && cbxCOLOR.SelectedItem != null)
+            {
+                try
+                {
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(Application.StartupPath + "\\Buttons.xml");
+                    XmlNode xmlNode = xmldoc.DocumentElement;
+                    XmlElement xmlElement = xmldoc.CreateElement("button");
+                    xmlElement.InnerXml = string.Format("<URL>{0}</URL><TEXT>{1}</TEXT><COLOR>{2}</COLOR>", txtURL.Text, txtTEXT.Text, cbxCOLOR.SelectedItem.ToString());
+                    xmlNode.AppendChild(xmlElement);
+                    xmldoc.Save(Application.StartupPath + "\\Buttons.xml");
+                    MessageBox.Show("Shortcut Added.");
+                    createButtons();
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                return;
+
+            }
+            MessageBox.Show("There was an error please check inputs.");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.FromName(cbxCOLOR.SelectedItem.ToString());
+        }
+    }
+}
